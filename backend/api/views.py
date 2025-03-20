5a50911e733022a3067a8e540dbc50c9cfe74ba8
zedaenious <matt@zelenak.me>
2025-03-20 15:01:17 -0400

    CreateUserView & set backend urls for user reg & token work

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+  # make sure user does not already exist
+  queryset = User.objects.all()
+  # necessary data (username, password)
+  serializer_class = User
+  # allow unauthenticated users to use this view
+  permission_classes = [AllowAny]
